cdb032c1b6e3f77e6a42386d22323db5f0af3b4c
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-05-29 19:30:04 +0200

    Fits test with two layer model.

--- edwin_groundwater_model/groundwater_MODFLOW.py
+++ edwin_groundwater_model/groundwater_MODFLOW.py
@@ -23,6 +23,7 @@ class GroundwaterModflow(object):
         # groundwater head (unit: m) for all layers
         for i in range(1, self.number_of_layers+1):
             var_name = 'groundwaterHeadLayer'+str(i)
+            print var_name
             result[var_name] = vars(self)[var_name]
         
         return result
