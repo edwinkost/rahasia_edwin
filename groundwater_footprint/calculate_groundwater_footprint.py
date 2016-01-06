d8e10c1417b770902818778a8fead692ca77c124
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-01-06 20:37:48 +0100

    Calculate groundwater foot print.

--- groundwater_footprint/calculate_groundwater_footprint.py
+++ groundwater_footprint/calculate_groundwater_footprint.py
@@ -50,7 +50,7 @@ cell_area = sedimentary_basin * cell_area
 class_map    = pcr.ifthen(sedimentary_basin > 0, class_map)
 
 # fraction for groundwater recharge to be reserved to meet the environmental flow
-fraction_reserved_recharge = pcr.readmap("/nfsarchive/edwin-emergency-backup-DO-NOT-DELETE/rapid/edwin/05min_runs_results/2015_04_27/non_natural_2015_04_27/global/analysis/reservedrecharge/fraction_reserved_recharge10.5min.map")
+fraction_reserved_recharge = pcr.readmap("/nfsarchive/edwin-emergency-backup-DO-NOT-DELETE/rapid/edwin/05min_runs_results/2015_04_27/non_natural_2015_04_27/global/analysis/reservedrecharge/minimum_fraction_reserved_recharge10.5min.map")
 # - extrapolation
 fraction_reserved_recharge = pcr.cover(fraction_reserved_recharge, \
                                        pcr.windowaverage(fraction_reserved_recharge, 0.5))
@@ -67,10 +67,10 @@ fraction_reserved_recharge = pcr.cover(fraction_reserved_recharge, \
 fraction_reserved_recharge = pcr.cover(fraction_reserved_recharge, \
                                        pcr.windowaverage(fraction_reserved_recharge, 0.5))
 fraction_reserved_recharge = pcr.cover(fraction_reserved_recharge, 0.1)
-# - set minimum value to 0.1
-fraction_reserved_recharge = pcr.max(0.1, fraction_reserved_recharge)
-# - set maximum value to 0.9
-fraction_reserved_recharge = pcr.min(0.9, fraction_reserved_recharge)
+# - set minimum value to 0.10
+fraction_reserved_recharge = pcr.max(0.10, fraction_reserved_recharge)
+# - set maximum value to 0.75
+fraction_reserved_recharge = pcr.min(0.75, fraction_reserved_recharge)
 
 # areal_groundwater_abstraction (unit: m/year)
 groundwater_abstraction = pcr.cover(pcr.readmap("/nfsarchive/edwin-emergency-backup-DO-NOT-DELETE/rapid/edwin/05min_runs_results/2015_04_27/non_natural_2015_04_27/global/analysis/avg_values_1990_to_2010/totalGroundwaterAbstraction_annuaTot_output_1990to2010.map"), 0.0)
